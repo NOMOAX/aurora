4a8639287355073e83a5dbc7886838932e363f76
谢凯文 <844038603@qq.com>
2025-04-24 11:53:52 +0800

    no message

---
+++ Runtime/CommentAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Aurora
+{
+    /// <summary>
+    /// 用于不支持 XML 文档注释的程序成员。
+    /// </summary>
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = false)]
+    public sealed class CommentAttribute : Attribute
+    {
+        /// <summary>
+        /// 注释。
+        /// </summary>
+        public readonly string Comment;
+
+        /// <summary>
+        /// 初始化 <see cref="CommentAttribute"/> 类的新实例。
+        /// </summary>
+        /// <param name="comment">注释。</param>
+        public CommentAttribute(string comment)
+        {
+            Comment = comment;
+        }
+    }
+}
