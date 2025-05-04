ec501ed347641999b425ee53cfffe66aaa9615e0
谢凯文 <844038603@qq.com>
2025-05-04 19:46:42 +0800

    no message

---
+++ Runtime/Interpolations/Interpolation.cs
@@ -0,0 +1,163 @@
+﻿namespace Aurora.Interpolations
+{
+    /// <summary>
+    /// 插值方式。
+    /// </summary>
+    public enum Interpolation
+    {
+        /// <summary>
+        /// 线性插值。
+        /// </summary>
+        Linear,
+
+        /// <summary>
+        /// 缓入正弦插值。
+        /// </summary>
+        InSine,
+
+        /// <summary>
+        /// 缓出正弦插值。
+        /// </summary>
+        OutSine,
+
+        /// <summary>
+        /// 缓入缓出正弦插值。
+        /// </summary>
+        InOutSine,
+
+        /// <summary>
+        /// 缓入二次插值。
+        /// </summary>
+        InQuad,
+
+        /// <summary>
+        /// 缓出二次插值。
+        /// </summary>
+        OutQuad,
+
+        /// <summary>
+        /// 缓入缓出二次插值。
+        /// </summary>
+        InOutQuad,
+
+        /// <summary>
+        /// 缓入三次插值。
+        /// </summary>
+        InCubic,
+
+        /// <summary>
+        /// 缓出三次插值。
+        /// </summary>
+        OutCubic,
+
+        /// <summary>
+        /// 缓入缓出三次插值。
+        /// </summary>
+        InOutCubic,
+
+        /// <summary>
+        /// 缓入四次插值。
+        /// </summary>
+        InQuart,
+
+        /// <summary>
+        /// 缓出四次插值。
+        /// </summary>
+        OutQuart,
+
+        /// <summary>
+        /// 缓入缓出四次插值。
+        /// </summary>
+        InOutQuart,
+
+        /// <summary>
+        /// 缓入五次插值。
+        /// </summary>
+        InQuint,
+
+        /// <summary>
+        /// 缓出五次插值。
+        /// </summary>
+        OutQuint,
+
+        /// <summary>
+        /// 缓入缓出五次插值。
+        /// </summary>
+        InOutQuint,
+
+        /// <summary>
+        /// 缓入指数插值。
+        /// </summary>
+        InExpo,
+
+        /// <summary>
+        /// 缓出指数插值。
+        /// </summary>
+        OutExpo,
+
+        /// <summary>
+        /// 缓入缓出指数插值。
+        /// </summary>
+        InOutExpo,
+
+        /// <summary>
+        /// 缓入圆形插值。
+        /// </summary>
+        InCirc,
+
+        /// <summary>
+        /// 缓出圆形插值。
+        /// </summary>
+        OutCirc,
+
+        /// <summary>
+        /// 缓入缓出圆形插值。
+        /// </summary>
+        InOutCirc,
+
+        /// <summary>
+        /// 缓入回弹插值。
+        /// </summary>
+        InBack,
+
+        /// <summary>
+        /// 缓出回弹插值。
+        /// </summary>
+        OutBack,
+
+        /// <summary>
+        /// 缓入缓出回弹插值。
+        /// </summary>
+        InOutBack,
+
+        /// <summary>
+        /// 缓入弹性插值。
+        /// </summary>
+        InElastic,
+
+        /// <summary>
+        /// 缓出弹性插值。
+        /// </summary>
+        OutElastic,
+
+        /// <summary>
+        /// 缓入缓出弹性插值。
+        /// </summary>
+        InOutElastic,
+
+        /// <summary>
+        /// 缓入弹跳插值。
+        /// </summary>
+        InBounce,
+
+        /// <summary>
+        /// 缓出弹跳插值。
+        /// </summary>
+        OutBounce,
+
+        /// <summary>
+        /// 缓入缓出弹跳插值。
+        /// </summary>
+        InOutBounce
+    }
+}
